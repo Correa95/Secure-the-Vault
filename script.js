34c8a5b98e01922bf82937febe793cfc69ab7c38
Correa95 <mathewcorrea19@gmail.com>
2024-07-22 13:50:58 -0400

    secure vault workshop

---
+++ script.js
@@ -0,0 +1,11 @@
+// passcode massage to access vault
+const message =
+  "You have received this message because you have been chosen to open an important vault. Here is the secret combination";
+
+//   vault passcode combination numbers
+const num1 = 2 * 5;
+const num2 = 20 + 20;
+const num3 = 50 - 11;
+
+// Dialog box to display vault code
+alert(`${message} ${num1} ${num2} ${num3}`);
